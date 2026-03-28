078494d23b4964bf4e0360bc4a1eee072a64e525
mudita149 <mudita.shukla14@gmail.com>
2026-03-29 04:56:35 +0530

    server.js updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,21 @@
 import React, { useContext } from 'react'
-import { Navigate, Route,Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import ProfilePage from './pages/ProfilePage'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from 'react-hot-toast'
 import { AuthContext } from '../context/AuthContext'
 //import ProfilePage from './pages/ProfilePage'
 const App = () => {
-  const {authUser} = useContext(AuthContext)
+  const { authUser } = useContext(AuthContext)
   return (
-    <div className="bg-[url('./src/assets/bgImage.jpg')]
+    <div className="bg-[url('/bgImage.jpg')]
     bg-contain">
-      <Toaster/>
+      <Toaster />
       <Routes>
-        <Route path='/' element= {authUser?<HomePage/>: <Navigate to="/login"/>}/>
-        <Route path='/login' element= {!authUser?<LoginPage/>: <Navigate to="/"/>}/>
-        <Route path='/profile' element= {authUser?<ProfilePage/>:<Navigate to="/login"/> }/>
+        <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
     </div>
   )
--- server/server.js
+++ server/server.js
@@ -55,6 +55,9 @@ app.use("/api/messages", messageRouter);
 
 // ── Start Server ───────────────────────────────────────────────────────────
 await connectDB();
-
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+if(process.env.NODE_ENV !=="production"){
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+}
+// export server for vercel deployment
+export default server;
